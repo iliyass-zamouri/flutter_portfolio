eb38440af0089a0186c1478ffc23c9ecd5b36d8b
Iliyass Zamouri <iliyass.zamouri@gmail.com>
2022-02-06 19:07:21 +0100

    skills widget

---
+++ lib/widgets/skills_widget.dart
@@ -0,0 +1,134 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:chips_choice/chips_choice.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_improved_scrolling/flutter_improved_scrolling.dart';
+import 'package:flutter_portfolio/models/skill.dart';
+import 'package:flutter_portfolio/state_provider.dart';
+import 'package:flutter_portfolio/styles/text_styles.dart';
+import 'package:provider/provider.dart';
+
+class SkillsWidget extends StatefulWidget {
+  final String title;
+  final List<String> categories;
+  final List<Skill> skills;
+  const SkillsWidget(
+      {Key? key,
+      required this.title,
+      required this.categories,
+      required this.skills})
+      : super(key: key);
+
+  @override
+  State<SkillsWidget> createState() => _SkillsWidgetState();
+}
+
+String selectedCategory = "All";
+late List<Skill> filtredList;
+
+final controller = ScrollController();
+
+class _SkillsWidgetState extends State<SkillsWidget> {
+  @override
+  void initState() {
+    super.initState();
+    widget.categories.insert(0, "All");
+    filtredList = widget.skills;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaData = MediaQuery.of(context);
+    StateProvider provider = Provider.of<StateProvider>(context);
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      width: mediaData.size.width <= 720
+          ? mediaData.size.width
+          : mediaData.size.width * 0.5,
+      decoration: BoxDecoration(
+          color: provider.colorPalette.secondColor,
+          boxShadow: [
+            BoxShadow(
+                color: provider.colorPalette.thirdColor.withOpacity(0.2),
+                blurRadius: 1,
+                spreadRadius: 1,
+                offset: const Offset(0.1, 0.2))
+          ],
+          borderRadius: BorderRadius.circular(7)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(widget.title,
+              overflow: TextOverflow.ellipsis,
+              style: Styles.tileTitle.copyWith(
+                  color: provider.colorPalette.thirdColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600)),
+          const SizedBox(height: 5),
+          Container(
+            margin: const EdgeInsets.all(5),
+            child: ChipsChoice<String>.single(
+                clipBehavior: Clip.antiAlias,
+                value: selectedCategory,
+                spinnerColor: provider.colorPalette.secondColor,
+                choiceActiveStyle: C2ChoiceStyle(
+                    color: provider.colorPalette.mainColor,
+                    borderColor: provider.colorPalette.mainColor,
+                    brightness:
+                        provider.isDark ? Brightness.dark : Brightness.light),
+                choiceStyle: C2ChoiceStyle(
+                    labelStyle: Styles.miniPlayerTile
+                        .copyWith(color: provider.colorPalette.thirdColor),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 12),
+                    brightness:
+                        provider.isDark ? Brightness.dark : Brightness.light),
+                onChanged: (choice) {
+                  if (choice == 'All') {
+                    setState(() {
+                      filtredList = widget.skills;
+                      selectedCategory = choice;
+                    });
+                  } else {
+                    filtredList = [];
+                    for (Skill element in widget.skills) {
+                      if (element.category == choice) {
+                        filtredList.add(element);
+                      }
+                    }
+                    selectedCategory = choice;
+                    setState(() {});
+                  }
+                },
+                choiceItems: widget.categories
+                    .map((e) => C2Choice(value: e, label: e))
+                    .toList()),
+          ),
+          Flexible(
+            child: SingleChildScrollView(
+              child: ListView.separated(
+                  shrinkWrap: true,
+                  // physics: const NeverScrollableScrollPhysics(),
+                  itemCount: filtredList.length,
+                  separatorBuilder: (context, index) {
+                    return Styles.divider(provider.colorPalette.thirdColor);
+                  },
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        filtredList[index].name,
+                        style: Styles.textSubTitle.copyWith(
+                            color: provider.colorPalette.thirdColor,
+                            fontSize: 18),
+                      ),
+                    );
+                  }),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
